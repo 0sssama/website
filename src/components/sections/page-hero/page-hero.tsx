ba0836ae589bbfd386860a237a910116c96c53f0
Oussama Labrahmi <labrahmioussama@gmail.com>
2024-03-08 22:53:25 +0100

    chore: add squares & buttons to `PageHeroSection`

--- src/components/sections/page-hero/page-hero.tsx
+++ src/components/sections/page-hero/page-hero.tsx
@@ -1,14 +1,22 @@
+import { Button } from '@/components/ui/button';
+
 import type { PageHeroProps } from './page-hero.types';
 
-export default function PageHeroSection({
-  title,
-  description,
-  // buttons
-}: PageHeroProps) {
+export default function PageHeroSection({ title, description, buttons }: PageHeroProps) {
   return (
-    <section className="relative w-full">
-      <h1>{title}</h1>
-      <h3>{description}</h3>
+    <section className="relative w-full pb-8 pt-28 md:pb-16 md:pt-40">
+      <div className="container">
+        <h1 className="gradient-title text-3xl max-md:mb-2 md:text-4xl md:leading-[75px]">{title}</h1>
+        <p className="text-sm font-light text-zinc-300 md:max-w-[80%]">{description}</p>
+        {Array.isArray(buttons) && (
+          <div className="mt-4 flex w-full items-center gap-2 md:mt-6">
+            {buttons.map((button, index) => (
+              <Button key={index} {...button} />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="pointer-events-none absolute -top-20 bottom-0 left-0 right-0 z-[-1] rotate-180 bg-[linear-gradient(to_right,#fafafa_1px,transparent_1px),linear-gradient(to_bottom,#fafafa_1px,transparent_1px)] bg-[size:4.03rem_4.1rem] opacity-5 [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
     </section>
   );
 }
