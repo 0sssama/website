0ccc7a6a25c3e399d83fd0284b8f70555f05049c
Oussama Labrahmi <oussama.labrahmi@bejamas.io>
2025-04-30 19:20:36 +0100

    chore: add declaration file

---
+++ declaration.d.ts
@@ -0,0 +1,29 @@
+declare module '*.webp' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const value: any;
+  export = value;
+}
+
+declare module '*.svg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const value: any;
+  export = value;
+}
+
+declare module '*.png' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const value: any;
+  export = value;
+}
