b76a8277f16b042941f71a4ececfbe5355e645f3
Oussama Labrahmi <oussama.labrahmi@bejamas.io>
2025-04-30 15:30:26 +0100

    chore: bump packages

--- src/components/elements/toaster/toaster.tsx
+++ src/components/elements/toaster/toaster.tsx
@@ -8,26 +8,7 @@ import type { ToasterProps } from './toaster.types';
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = 'system' } = useTheme();
 
-  return (
-    <Sonner
-      theme={theme as ToasterProps['theme']}
-      closeButton
-      richColors
-      position="bottom-right"
-      //   className="toaster group"
-      //   toastOptions={{
-      // classNames: {
-      //   toast:
-      //     'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
-      //   description: 'group-[.toast]:text-muted-foreground',
-      //   actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
-      //   cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
-      //   error: 'group-[.toast]:bg-destructive group-[.toast]:text-destructive-foreground',
-      // },
-      //   }}
-      {...props}
-    />
-  );
+  return <Sonner theme={theme as ToasterProps['theme']} closeButton richColors position="bottom-right" {...props} />;
 };
 
 export default Toaster;
