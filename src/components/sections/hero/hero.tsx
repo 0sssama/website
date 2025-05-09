39127d0857bf92f014238a3e12673a11da5dbf8d
Oussama Labrahmi <oussama.labrahmi@bejamas.io>
2025-05-09 20:11:40 +0100

    chore: add hero section animation

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { motion } from 'motion/react';
+
 import { cn } from '@/utils/cn';
 import { Button } from '@/components/elements/button';
 import { Markdown } from '@/components/molecules/markdown';
@@ -6,25 +10,43 @@ import type { HeroProps } from './hero.types';
 import { AuroraBackground } from './components/aurora-background';
 import ParsedTitle from './components/parsed-title';
 
+const transition = { duration: 1, ease: [0.25, 0.1, 0.25, 1] };
+
+const variants = {
+  hidden: { filter: 'blur(10px)', transform: 'translateY(20%)', opacity: 0 },
+  visible: { filter: 'blur(0)', transform: 'translateY(0)', opacity: 1 },
+};
+
 export default function Hero({ heading, description, buttons, className }: HeroProps) {
   return (
     <AuroraBackground>
-      <section
+      <motion.section
         className={cn(
-          'container flex flex-col items-center justify-center pt-28 pb-10 md:pt-40 md:pb-24 md:text-center',
+          'container flex flex-col items-center justify-center pt-28 pb-10 md:pt-40 md:pb-56 md:text-center',
           className,
         )}
+        initial="hidden"
+        whileInView="visible"
+        transition={{ staggerChildren: 0.225 }}
       >
-        <h1 className="text-5xl leading-none text-balance sm:text-6xl md:text-7xl">
+        <motion.h1
+          className="text-5xl leading-none text-balance sm:text-6xl md:text-7xl"
+          transition={transition}
+          variants={variants}
+        >
           <ParsedTitle>{heading}</ParsedTitle>
-        </h1>
-        <div className="mt-6 mb-8 md:max-w-xl">
+        </motion.h1>
+        <motion.div className="mt-6 mb-8 md:max-w-xl" transition={transition} variants={variants}>
           <Markdown className="leading-none! *:text-base [&_*]:text-zinc-700!" content={description} />
-        </div>
-        <div className="flex w-full items-center gap-2 md:justify-center md:gap-4">
+        </motion.div>
+        <motion.div
+          className="flex w-full items-center gap-2 md:justify-center md:gap-4"
+          transition={transition}
+          variants={variants}
+        >
           {Array.isArray(buttons) && buttons.map((button, index) => <Button key={index} {...button} />)}
-        </div>
-      </section>
+        </motion.div>
+      </motion.section>
     </AuroraBackground>
   );
 }
